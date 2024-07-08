aac5b9be33cf5f85bd311fc73ea93e951b2bea32
reshetnikov_xiv@mail.ru <reshetnikov_xiv@mail.ru>
2024-07-08 11:10:30 +0000

    Zip file uploaded

--- src/scripts/Holidays/holiday.js
+++ src/scripts/Holidays/holiday.js
@@ -30,7 +30,7 @@ function holiday($context, countersArray, currentState){
     
     var date = currentDate();
     
-    var allHolidaysDates = "7/6/2024 9/6/2024 10/6/2024 11/6/2024 12/6/2024";
+    var allHolidaysDates = "2/7/2024 3/7/2024 4/7/2024 1/7/2024 12/6/2024";
     
     var allHolidaysArray = allHolidaysDates.split(' ');
     
@@ -40,7 +40,7 @@ function holiday($context, countersArray, currentState){
         
         if(currentState == "/Доступные биржи" && countersArray[0] == true){
             
-        $reactions.answer("12 июня торги и расчёты на Московской бирже не проводятся. На иностранных биржах торги проводятся в стандартном режиме.");
+        $reactions.answer("4 июля – день Независимости в США. Торги и расчёты на американских биржах не проводятся.");
         countersArray[0] = false;
         return 0;    
         
@@ -48,7 +48,7 @@ function holiday($context, countersArray, currentState){
         
         if(currentState == "/Время торгов" && countersArray[1] == true){
             
-        $reactions.answer("12 июня торги и расчёты на Московской бирже не проводятся. На иностранных биржах торги проводятся в стандартном режиме.");
+        $reactions.answer("4 июля – день Независимости в США. Торги и расчёты на американских биржах не проводятся.");
         countersArray[1] = false;
         return 0;    
         
@@ -56,17 +56,17 @@ function holiday($context, countersArray, currentState){
         
         if(currentState == "/Режим расчетов" && countersArray[2] == true){
             
-        $reactions.answer("12 июня торги и расчёты на Московской бирже не проводятся. На иностранных биржах торги проводятся в стандартном режиме.");
+        $reactions.answer("4 июля – день Независимости в США. Торги и расчёты на американских биржах не проводятся.");
         countersArray[2] = false;
         return 0;    
         
         }
         
-        if(currentState == "/Время работы"){
+        // if(currentState == "/Время работы"){
             
-        $reactions.answer("12 июня – нерабочий день для всех офисов компании Фина'м.");
-        return 0;    
-        }
+        // $reactions.answer("12 июня – нерабочий день для всех офисов компании Фина'м.");
+        // return 0;    
+        // }
         
         else {
             
